6808c34a70b42a3c60d6013b2f43cf6b530ccd0f
Goodetho <91574451+Goodetho@users.noreply.github.com>
2021-12-02 16:25:55 -0500

    Update main.h

--- finalproject/main.h
+++ finalproject/main.h
@@ -1,11 +1,9 @@
 #ifndef MAIN_H_
 #define MAIN_H_
-#include "LCD.h"
 #include <avr/interrupt.h>
 //volatile global variables
 volatile char rx_buffer[25];  //stores the string to be displayed on the LCD
 volatile uint8_t rx_char;    //stores value of UDR0
-volatile uint8_t LCD_update; //boolean to differentiate functions
 volatile uint8_t array_index;  //keeps track of place in rx_buffer
 
 
