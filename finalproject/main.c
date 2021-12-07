9d77112150dc5e197e9a5021a4f9a944f99928ad
gjohn2503 <john2503@purdue.edu>
2021-12-07 14:58:41 -0500

    added coments
    added comments and made minor changes

--- finalproject/main.c
+++ finalproject/main.c
@@ -33,22 +33,22 @@ int main(void)
 	uint16_t value_two = 0;
 	uint8_t locked = 1;
 	
-	uint16_t count = 10000;
+	uint16_t count = 10000; //initialize count variable to track the base
 	
 	uint16_t num = 0;
 	
-	uint16_t arm_pos_one = 0;
-	uint16_t base_pos_one = 0;
+	uint16_t arm_pos_one = 0; //Value to save arm position one
+	uint16_t base_pos_one = 0; //Value to save base position one
 
-	uint16_t arm_pos_two = 0;
-	uint16_t base_pos_two = 0;
+	uint16_t arm_pos_two = 0; //Value to save arm position two
+	uint16_t base_pos_two = 0; //Value to save base position two
 	
 	char pass[20] = {"password"};
 	
 	TCCR1A|=(1<<COM1A1)|(1<<COM1B1)|(1<<WGM11);        //NON Inverted PWM
 	TCCR1B|=(1<<WGM13)|(1<<WGM12)|(1<<CS11)|(1<<CS10); //PRESCALER=64 MODE 14(FAST PWM)
 
-	ICR1=5000;  //fPWM=50Hz (Period = 20ms Standard).
+	ICR1=4999;  //fPWM=50Hz (Period = 20ms Standard).
 	
 	OCR1A = 250;
 	OCR1B = 250;
@@ -70,7 +70,7 @@ int main(void)
 	}
 	UART_outstring("Configuration mode enabled");
 	
-	while ((PINC & 0x01))
+	while ((PINC & 0x01)) //home arm before starting
 	{
 		Stepper_Position('W',n);
 	}
@@ -85,7 +85,7 @@ int main(void)
 			{
 				while (PINA & Wave_Step)
 				{
-				Stepper_Position('W',n); //Wave rotation
+				Stepper_Position('W',n); //turn right
 				count++;
 				}
 
@@ -93,7 +93,7 @@ int main(void)
 			}
 			case Full_Step:
 			{
-				Stepper_Position('F',n); //Full rotation
+				Stepper_Position('F',n); //turn left
 				count--;
 				break;
 			}
@@ -101,8 +101,8 @@ int main(void)
 			
 			case Position:
 			{
-				count = 1000;
-				while ((PINC & 0x01))
+				count = 10000;
+				while ((PINC & 0x01)) //home arm
 				{
 					Stepper_Position('W',n);
 				}
@@ -115,25 +115,25 @@ int main(void)
 		
 		PORTK = 0x00; //clear output when done
 		
-			value = ten_bit_ADC(0);
+			value = ten_bit_ADC(0); //Convert ADC input to output to end effector
 			value = ((value*2)+97);
-			OCR1A = value;
-			value_two = ten_bit_ADC(1);
+			OCR1A = value; //output value
+			value_two = ten_bit_ADC(1); //Convert ADC input to output to arm height
 			value_two = ((value_two*2)+97);
-			OCR1B = value_two;
+			OCR1B = value_two; //output value
 			
 			
 				
-		while (PINC & 0x02)
+		while (PINC & 0x02) //if switch one is activated
 		{
 			
-			if (PINA & 0x01)
+			if (PINA & 0x01) //record position one
 			{
 				arm_pos_one = value_two;
 				base_pos_one = count;	
 			}
 			
-			if (PINA & 0x02)
+			if (PINA & 0x02) //record position two
 			{
 				arm_pos_two = value_two;
 				base_pos_two = count;
@@ -144,7 +144,7 @@ int main(void)
 		if (PINC & 0x08) //Enter Automatic mode
 		{
 			OCR1A = 475; //retract arm
-			if (value_two>arm_pos_one)
+			if (value_two>arm_pos_one) //if the arm is above the point move down
 			{
 				for (num = value_two; num>arm_pos_one; num--)
 				{
@@ -152,7 +152,7 @@ int main(void)
 					_delay_ms(10);
 				}
 			}
-			if (value_two < arm_pos_one)
+			if (value_two < arm_pos_one) //if the arm is below the point move up
 			{
 				for (num = value_two; num<arm_pos_one; num++)
 				{
@@ -163,7 +163,7 @@ int main(void)
 			value_two = num;
 			
 			
-			if (count>base_pos_one)
+			if (count>base_pos_one) //if base is past save point move to position
 			{
 				for (num = count; num > base_pos_one; num--)
 				{
@@ -172,7 +172,7 @@ int main(void)
 					_delay_ms(10);
 				}
 			}
-			if (value_two < base_pos_one)
+			if (value_two < base_pos_one) //if base is below position move up to position
 			{
 				for (num = count; num < base_pos_one; num++)
 				{
@@ -194,6 +194,7 @@ int main(void)
 			_delay_ms(100);
 			//move to next spot
 			
+			//code below is same as above but for moving to the second position
 			if (count>base_pos_two)
 			{
 				for (num = count; num > base_pos_two; num--)
@@ -229,8 +230,8 @@ int main(void)
 					_delay_ms(10);
 				}
 			}
-			OCR1A = 475;
-			_delay_ms(500);
+			OCR1A = 500;
+			_delay_ms(1000);
 			value_two = num;
 			for (num = arm_pos_one; num>250; num--)
 			{
@@ -238,14 +239,9 @@ int main(void)
 				_delay_ms(10);
 			}
 			
-			while ((PINC & 0x01))
-			{
-				Stepper_Position('W',n);
-			}
-			Stepper_Position('F',h);
-			count = 10000;
+			OCR1A = 300;
 			
-			while(PINC & 0x08)
+			while(PINC & 0x08) //wait for switch to be deactivated
 			{
 			}
 			
